4506f175df63849aa7168a098646423a5bad21a5
SCREECH115 <arekw99@gmail.com>
2022-09-03 03:53:56 +0200

    Replaced alerts by Winner Text

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const sleep = (milliseconds) => {
 for (let i = 0; i < 9; i++) {
   const tile = $.parseHTML(`<div class="tile" id="tile_${i}"></div>`);
   $(tile).on("click", async (e) => {
+    $(".winner").text("");
     if (numbers[i]) return;
     click = click == "O" ? "X" : "O";
     numbers[i] = click;
@@ -18,7 +19,7 @@ for (let i = 0; i < 9; i++) {
       numbers = Array(9).fill(null);
       $(".tile").text("");
     } else if (numbers.every((e) => e)) {
-      alert("Draw");
+      $(".winner").text("Draw!");
       numbers = Array(9).fill(null);
       $(".tile").text("");
     }
@@ -33,7 +34,7 @@ const checkWinner = () => {
       numbers[i * 3] == numbers[i * 3 + 2] &&
       numbers[i * 3]
     ) {
-      alert(`${numbers[i * 3]} wins!`);
+      $(".winner").text(`${numbers[i * 3]} wins!`);
       return true;
     }
     if (
@@ -41,24 +42,20 @@ const checkWinner = () => {
       numbers[i] == numbers[i + 6] &&
       numbers[i]
     ) {
-      alert(`${numbers[i]} wins!`);
+      $(".winner").text(`${numbers[i]} wins!`);
       return true;
     }
   }
 
   if (numbers[0] == numbers[4] && numbers[0] == numbers[8] && numbers[0]) {
-    alert(`${numbers[0]} wins!`);
+    $(".winner").text(`${numbers[0]} wins!`);
     return true;
   }
 
   if (numbers[2] == numbers[4] && numbers[2] == numbers[6] && numbers[2]) {
-    alert(`${numbers[2]} wins!`);
+    $(".winner").text(`${numbers[2]} wins!`);
     return true;
   }
 
   return false;
-
-  // if (numbers.slice(0, 3).every((e) => e == "X")) {
-  //   alert("X wins");
-  // }
 };
